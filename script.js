90dadae390a3cc41911ea5cecf565834143aa6fb
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2021-07-16 00:03:02 +0300

    16.07 0:02

--- script.js
+++ script.js
@@ -42,4 +42,102 @@ $(document).ready(function(){
 	})
 
 
+	$('#autoparsingSlider').slick({
+	  dots: true,
+	  arrows: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 6,
+	  slidesToScroll: 2,
+	  responsive: [
+	    {
+	      breakpoint: 1440,
+	      settings: {
+	        slidesToShow: 5,
+	        slidesToScroll: 3,
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 1380,
+	      settings: {
+	        slidesToShow: 4,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 680,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	  ]
+	});
+
+	$('#youtubeSlider').slick({
+	  dots: true,
+	  arrows: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 4,
+	  slidesToScroll: 2,
+	  responsive: [
+	    {
+	      breakpoint: 1440,
+	      settings: {
+	        slidesToShow: 5,
+	        slidesToScroll: 3,
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 1380,
+	      settings: {
+	        slidesToShow: 4,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 680,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	  ]
+	});
+
+
+
 });
